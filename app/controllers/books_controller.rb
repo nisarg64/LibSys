b1e1ea3ce3254b449a8d7972d054669c141f0424
Jitendra Jain <jsjain@ncsu.edu>
2015-09-26 11:43:04 -0400

    Add Error messages to library member signup. Change default checked radio button for search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @search_params = {:keyword=>"",:checked_out=>"false"}
+    @search_params = {:keyword=>'',:checked_out=>'all'}
   end
 
   def show
